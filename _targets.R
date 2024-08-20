2d5eb8c3ef9552ba0cb9d16a85f8eb008f720582
Jack Hendrix <55209731+jghendrix@users.noreply.github.com>
2024-08-20 13:03:29 -0230

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -1,5 +1,44 @@
 #Targets workflow
 
-target_data_import <- c(
-  tar_target()
+# Source ------------------------------------------------------------------
+targets::tar_source('R')
+
+
+
+# Options -----------------------------------------------------------------
+tar_option_set(format = 'qs')
+
+# Defining study area
+tar_list <- c(
+  tar_target(
+  bb,
+  c(xmin = -54.3533,
+          ymin = 49.5194,
+          xmax = -53.954220,
+          ymax = 49.763834)),
+  
+  tar_target(
+    utm,
+    st_crs('+proj=utm +zone=21 ellps=WGS84')
+  ),
+
+# Get data from cloud
+
+tar_target(
+  land,
+  get_coastline(bb, utm)),
+
+tar_target(
+  lakes,
+  get_lakes(bb, utm)
 )
+)
+
+
+
+
+
+
+# Targets: all ------------------------------------------------------------
+# Automatically grab and combine all the "targets_*" lists above
+lapply(grep('targets', ls(), value = TRUE), get)
